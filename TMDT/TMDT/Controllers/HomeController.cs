d658d97452627868cbb423e04e5dce11b72200cc
songtrenbo <songtrenbo1@gmail.com>
2021-11-07 10:42:39 +0700

    page trang chu, chi tiet san pham

---
+++ TMDT/TMDT/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDT.Models;
+
+namespace TMDT.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBLaptopEntities database = new DBLaptopEntities();
+        public ActionResult Index()
+        {
+            ViewBag.thuonghieu = database.ThuongHieux.ToList();
+            var sanpham = database.SanPhams.ToList();
+            return View(sanpham);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TMDT/TMDT/Controllers/SanPhamController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDT.Models;
+
+namespace TMDT.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        DBLaptopEntities database = new DBLaptopEntities();
+        // GET: SanPham
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CT_SanPham(int MaSanPham)
+        {
+            var sanpham = database.SanPhams.Where(s => s.MaSanPham == MaSanPham).FirstOrDefault();
+            return View(sanpham);
+        }
+    }
+}
