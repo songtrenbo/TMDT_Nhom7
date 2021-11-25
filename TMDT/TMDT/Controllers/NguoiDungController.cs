a9fcb76a42b5fbff6014bb318c3ea91fcabad2ff
songtrenbo <songtrenbo1@gmail.com>
2021-11-25 13:39:24 +0700

    sua giao dien

--- TMDT/TMDT/Controllers/NguoiDungController.cs
+++ TMDT/TMDT/Controllers/NguoiDungController.cs
@@ -52,7 +52,7 @@ namespace TMDT.Controllers
             switch (checkUser.MaQuyen)
             {
                 case 1:
-                    return RedirectToAction("QLTaiKhoan", "Admin");
+                    return RedirectToAction("QLQuyen", "Admin");
                 case 2:
                     return RedirectToAction("QuanLyThuongHieu", "QuanLy");
                 case 3:
