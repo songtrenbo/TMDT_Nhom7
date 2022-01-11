f5504c596bc31ada311d56d30db222d19114f523
minhchi32 <tranminhchi0302@gmail.com>
2022-01-11 21:51:48 +0700

    advanced search

--- TMDT/TMDT/Controllers/SearchController.cs
+++ TMDT/TMDT/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,16 +12,51 @@ namespace TMDT.Controllers
     {
         // GET: Search
         DBLaptopEntities database = new DBLaptopEntities();
-        public ActionResult Index(string name)
+        public ActionResult Index(string name, int? page, int danhMucID = 0, int cpuID = 0, int loaiGPUID = 0, int sizeManHinhID = 0, int thuongHieuID = 0)
         {
-            //ViewBag.abc = database.ThuongHieux.Where(x=>x.MaThuongHieu == a).FirstOrDefault().TenThuongHieu;
-            //return View();
-            var result = database.SanPhams.Where(x => x.IsHide == false);
-            if (!String.IsNullOrEmpty(name))
+            int pageSize = 4;
+            int pageNum = (page ?? 1);
+
+
+            var danhmuc = database.DanhMucs.ToList();
+            var cpu = database.CPUs.ToList();
+            var loaiGPU = database.LoaiGPUs.ToList();
+            var sizeManHinh = database.SizeManHinhs.ToList();
+            var thuongHieu = database.ThuongHieux.ToList();
+            ViewBag.danhmuc = new SelectList(danhmuc, "MaDanhMuc", "TenDanhMuc"); ;
+            ViewBag.cpu = new SelectList(cpu, "MaCPU", "TenCPU"); ;
+            ViewBag.loaiGPU = new SelectList(loaiGPU, "MaLoaiGPU", "TenLoaiGPU"); ;
+            ViewBag.sizeManHinh = new SelectList(sizeManHinh, "MaSizeManHinh", "Size"); ;
+            ViewBag.thuongHieu = new SelectList(thuongHieu, "MaThuongHieu", "TenThuongHieu", thuongHieuID); ;
+            var listSP = database.SanPhams.Where(x => x.IsHide == false).OrderByDescending(x => x.NgayTao).ToList();
+            var result = new List<SanPham>(listSP);
+            if (danhMucID != 0)
             {
-            result = result.Where(x => x.TenSanPham.Contains(name));
+                result = result.Where(s => s.DanhMuc.MaDanhMuc == danhMucID).ToList();
+                ViewBag.danhMucID = danhMucID;
             }
-            return View(result.ToList());
+            if (cpuID != 0)
+            {
+                result = result.Where(s => s.CPU.MaCPU == cpuID).ToList();
+                ViewBag.cpuID = cpuID;
+            }
+            if (loaiGPUID != 0)
+            {
+                result = result.Where(s => s.LoaiGPU.MaLoaiGPU == loaiGPUID).ToList();
+                ViewBag.loaiGPUID = loaiGPUID;
+            }
+            if (sizeManHinhID != 0)
+            {
+                result = result.Where(s => s.SizeManHinh1.MaSizeManHinh == sizeManHinhID).ToList();
+                ViewBag.sizeManHinhID = sizeManHinhID;
+            }
+            if (thuongHieuID != 0)
+            {
+                result = result.Where(s => s.ThuongHieu.MaThuongHieu == thuongHieuID).ToList();
+                ViewBag.thuongHieuID = thuongHieuID;
+            }
+            ViewBag.result = result.Count();
+            return View(result.ToPagedList(pageNum, pageSize));
         }
     }
 }
